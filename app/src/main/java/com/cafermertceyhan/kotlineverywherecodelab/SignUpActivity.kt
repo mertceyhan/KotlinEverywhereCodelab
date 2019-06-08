d35a9c761d68bba9cc1ef33258ebd14f0e7c618e
mertceyhan <c.mertceyhan@hotmail.com>
2019-06-08 23:39:11 +0300

    Create SignUpActivity

---
+++ app/src/main/java/com/cafermertceyhan/kotlineverywherecodelab/SignUpActivity.kt
@@ -0,0 +1,12 @@
+package com.cafermertceyhan.kotlineverywherecodelab
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SignUpActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+    }
+}
